b02ebb7b460228c95ffe5580d8b9e3e075beaf4d
krishankantray <krishankant_rai@rediffmail.com>
2022-05-29 06:18:15 +0530

    global variable issue fixed

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,8 @@ import LayoutWrapper from '@/components/LayoutWrapper'
 import Twemoji from '@/components/Twemoji'
 
 function detectmobile() {
-  let a = global.navigator.userAgent || global.navigator.vendor || window.opera
+  let a = global.navigator?.userAgent || global.navigator?.vendor || global.window?.opera
+  if (!a) a = ''
   if (
     /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
       a
